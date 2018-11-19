094e80552c19879f0d40c828157fc152e39e804d
albertosml <albertosml@correo.ugr.es>
2018-11-19 20:46:49 +0100

    Doing facet index

--- StackOverflowSearcher/src/stackoverflowsearcher/IndexBuilder.java
+++ StackOverflowSearcher/src/stackoverflowsearcher/IndexBuilder.java
@@ -23,6 +23,9 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.facet.FacetField;
+import org.apache.lucene.facet.FacetsConfig;
+import org.apache.lucene.facet.taxonomy.directory.DirectoryTaxonomyWriter;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
@@ -37,12 +40,18 @@ public final class IndexBuilder {
     public Analyzer RcodeAnalyzer;
     private Similarity similarity;
     private static final String INDEX_DIRECTORY = "./index"; 
+    private static final String FACET_DIRECTORY = "./facet"; 
     private IndexWriter writer;
     public Map<String, Analyzer> analyzerPerField = new HashMap<String, Analyzer>();
     private List<String> palabras_codigo_r = Arrays.asList("for","if","else","function","while","case","break","do","try","catch","return",
     "objects","rm","assign","order","sort","numeric","character","integer");
+    private FacetsConfig fconfig;
+    private DirectoryTaxonomyWriter fwriter;
+    private String[] meses;
     
     public IndexBuilder(String ruta_preguntas, String ruta_respuestas, List<String[]> etiquetas) throws IOException {
+        this.meses = new String[]{"ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO",
+            "JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE"};
         this.similarity = new BM25Similarity();
         
         // Creamos un analizador de código R 
@@ -83,15 +92,31 @@ public final class IndexBuilder {
         // Abrimos directorio de índices
         FSDirectory dir = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
 
+        // Abrimos el directorio de facetas
+        FSDirectory factDir = FSDirectory.open(Paths.get(FACET_DIRECTORY));
+        
         // Configuramos índice al cual le vamos a pasar el StandardAnalyzer
         IndexWriterConfig config = new IndexWriterConfig(ana);
         config.setSimilarity(similarity); // Usamos como medida de similitud el Okapi BM25
+        config.setOpenMode(OpenMode.CREATE); // Configuramos el índice de forma que se cree un índice cada vez que se ejecute esta función
         
-        // Creamos un nuevo índice
-        config.setOpenMode(OpenMode.CREATE_OR_APPEND);
+        // Configuramos facetas
+        fconfig = new FacetsConfig(); 
+        
+        fconfig.setIndexFieldName("pregunta","facet_pregunta"); // La faceta pregunta la gestiona dentro del facet_pregunta
+        fconfig.setIndexFieldName("usuario","facet_usuario"); // La faceta usuario la gestiona dentro del facet_usuario
+        fconfig.setIndexFieldName("fecha","facet_fecha"); // La faceta fecha la gestiona dentro del facet_fecha
+        fconfig.setIndexFieldName("pregunta","facet_pregunta"); // La faceta pregunta la gestiona dentro del facet_pregunta
+        fconfig.setIndexFieldName("respuesta_aceptada","facet_respuesta_aceptada"); // La faceta respuesta_aceptada la gestiona dentro del facet_respuesta_aceptada
+        fconfig.setIndexFieldName("puntuacion", "facet_puntuacion"); // La faceta puntuacion la gestiona dentro del facet_puntuacion
+        
+        fconfig.setHierarchical("fecha", true); // Creamos una jerarquía para la faceta fecha
         
         // Construimos IndexWriter con los parámetros dados en config
         writer = new IndexWriter(dir, config); 
+        
+        // Construimos DirectoryTaxonomyWriter pásandole el directorio de facetas
+        fwriter = new DirectoryTaxonomyWriter(factDir);
     }
     
     public void indexDocuments(String ruta_preguntas, String ruta_respuestas, List<String[]> etiquetas) throws IOException {   
@@ -107,9 +132,9 @@ public final class IndexBuilder {
         while((d = csvreader.readNext()) != null) {
             Document doc = new Document();
             
-            //El ID y el OWNERID no lo almaceno, ya que es una número de usuario y no se estiman búsquedas por el campo
-            doc.add(new StringField("Id_q", d[0],Field.Store.NO));
-            doc.add(new StringField("OwnerUserId_q", d[1],Field.Store.NO));
+            // Incluimos los campos a indexar
+            doc.add(new StringField("Id_q", d[0],Field.Store.YES));
+            doc.add(new StringField("OwnerUserId_q", d[1],Field.Store.YES));
             doc.add(new StringField("CreationDate_q", d[2],Field.Store.YES));
             doc.add(new StringField("Score_q", d[3],Field.Store.YES));
             doc.add(new TextField("Title_q", d[4],Field.Store.YES));
@@ -123,7 +148,18 @@ public final class IndexBuilder {
               }  
             }
             
-            writer.addDocument(doc);
+            // Incluimos las facetas
+            doc.add(new FacetField("pregunta", "true"));
+            doc.add(new FacetField("usuario", d[1]));
+            
+            // Extraemos el mes y el año
+            String [] fecha;
+            fecha = d[2].split("-");
+            doc.add(new FacetField("fecha", fecha[0], meses[Integer.parseInt(fecha[1]) - 1]));
+            
+            doc.add(new FacetField("puntuacion", this.categorizeScore(Integer.parseInt(d[3]))));
+            
+            writer.addDocument(fconfig.build(fwriter, doc));
         }
         
         csvreader.close(); // Cerramos csv
@@ -139,8 +175,8 @@ public final class IndexBuilder {
         while((d = csvreader.readNext()) != null) {  
             Document doc = new Document();
                       
-            doc.add(new StringField("Id_a", d[0],Field.Store.NO));
-            doc.add(new StringField("OwnerUserId_a", d[1],Field.Store.NO));
+            doc.add(new StringField("Id_a", d[0],Field.Store.YES));
+            doc.add(new StringField("OwnerUserId_a", d[1],Field.Store.YES));
             doc.add(new StringField("CreationDate_a", d[2],Field.Store.YES));
             doc.add(new TextField("ParentId_a", d[4],Field.Store.NO));
             doc.add(new StringField("Score_a", d[3],Field.Store.YES));
@@ -153,9 +189,21 @@ public final class IndexBuilder {
               if(e.tagName().equals("code")){
                   doc.add(new TextField("Code_a", e.text(),Field.Store.YES));
               }  
-            }            
+            }          
             
-            writer.addDocument(doc);
+            // Incluimos las facetas
+            doc.add(new FacetField("pregunta", "false"));
+            doc.add(new FacetField("respuesta_aceptada", d[5]));
+            doc.add(new FacetField("usuario", d[1]));
+            
+            // Extraemos el mes y el año
+            String [] fecha;
+            fecha = d[2].split("-");
+            doc.add(new FacetField("fecha", fecha[0], meses[Integer.parseInt(fecha[1]) - 1]));
+            
+            doc.add(new FacetField("puntuacion", this.categorizeScore(Integer.parseInt(d[3]))));
+            
+            writer.addDocument(fconfig.build(fwriter, doc));
         }
         
         csvreader.close(); // Cerramos csv
@@ -172,4 +220,10 @@ public final class IndexBuilder {
         }
        
     }  
+    
+    private String categorizeScore(int score) {
+        if(score < 50) return "Bajo";
+        else if(score < 100) return "Medio";
+        else return "Alto";
+    }
 }
