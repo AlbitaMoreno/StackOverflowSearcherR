c2c6d91d89c914199ea2e33c0dd81365a4899f6e
albertosml <albertosml@correo.ugr.es>
2018-12-04 23:28:26 +0100

    Busqueda normal

--- StackOverflowSearcher/src/stackoverflowsearcher/ProcessQuery.java
+++ StackOverflowSearcher/src/stackoverflowsearcher/ProcessQuery.java
@@ -1,21 +1,22 @@
 package stackoverflowsearcher;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
+import javafx.util.Pair;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.TermQuery;
 
 public class ProcessQuery {
-    private static final String INDEX_DIRECTORY = "./index";
+    private static final String INDEX_DIRECTORY = "./../../index";
     private String line;
     public BooleanQuery query;
     
@@ -24,62 +25,54 @@ public class ProcessQuery {
     }
     
     public BooleanQuery procQuery() throws ParseException {
-        Map<String,Query> queries = new HashMap<String,Query>();
+        List<Query> queries = new ArrayList<>();
         
-        // Parseamos título de la pregunta
-        QueryParser parser = new QueryParser("Title_q", new StandardAnalyzer());
+        Query query;
+        QueryParser parser;
+        for(String t : this.line.split(" ")) {
+            // Parseamos título
+            parser = new QueryParser("Title", new StopAnalyzer());
+            query = parser.parse(t);
+            queries.add(query);
+            
+            // Parseamos cuerpo
+            parser = new QueryParser("Body", new StopAnalyzer());
+            query = parser.parse(t);
+            queries.add(query);
+            
+            // Parseamos código 
+            parser = new QueryParser("Code", new StopAnalyzer());
+            query = parser.parse(t);
+            queries.add(query);
+        }
+            
+        /*Query query;
+        for(String t : this.line.split(" ")) {
+            query = new TermQuery(new Term("Title", t));
+            queries.put("Title", query);
+            
+            System.out.println(query.toString());
+            
+            query = new TermQuery(new Term("Body", t));
+            queries.put("Body", query);
+            
+            System.out.println(query.toString());
+            
+            query = new TermQuery(new Term("Code", t));
+            queries.put("Code", query);
+            
+            System.out.println(query.toString());
+        }*/
         
-        Query query = parser.parse(line);
-        
-        queries.put("Title_q", query);
-        
-        // Parseamos cuerpo de la pregunta
-        parser = new QueryParser("Body_q", new StandardAnalyzer());
-        
-        query = parser.parse(line);
-        
-        queries.put("Body_q", query);
-        
-        // Parseamos código de la pregunta
-        parser = new QueryParser("Code_q", new StandardAnalyzer());
-        
-        query = parser.parse(line);
-        
-        queries.put("Code_q", query);
-        
-        // Parseamos título de la respuesta
-        parser = new QueryParser("Title_a", new StandardAnalyzer());
-    
-        query = parser.parse(line);
-        
-        queries.put("Title_a", query);
-        
-        // Parseamos cuerpo de la respuesta
-        parser = new QueryParser("Body_a", new StandardAnalyzer());
-        
-        query = parser.parse(line);
-        
-        queries.put("Body_a", query);
-        
-        // Parseamos código de la pregunta
-        parser = new QueryParser("Code_a", new StandardAnalyzer());
-        
-        query = parser.parse(line);
-        
-        queries.put("Code_a", query);
-        
+        // Construimos la consulta
         BooleanQuery.Builder bqbuilder = new BooleanQuery.Builder();
         
-        Iterator it = queries.entrySet().iterator();
-        while(it.hasNext()) {
-            Map.Entry e = (Map.Entry) it.next();
-            String field = (String) e.getKey();
-            Query q = (Query) e.getValue();
-            
+        for(Query q : queries) {
             BooleanClause bc = new BooleanClause(q, BooleanClause.Occur.SHOULD);
             bqbuilder.add(bc);
         }
         this.query = bqbuilder.build();
+        System.out.println(this.query.toString());
         
         return this.query;
     }
--- StackOverflowSearcher/src/stackoverflowsearcher/StackOverflowSearcher.java
+++ StackOverflowSearcher/src/stackoverflowsearcher/StackOverflowSearcher.java
@@ -1,13 +1,8 @@
 package stackoverflowsearcher;
 
-import com.opencsv.CSVReader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 /**
  *
@@ -17,21 +12,7 @@ public class StackOverflowSearcher {
     
     public static void main(String[] args) throws Exception { 
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-        boolean indexNotDefine = false;
-        // Extraer etiquetas
-        if(indexNotDefine){
-            // Abro csv
-            Reader reader = Files.newBufferedReader(Paths.get("./rquestions/Tags.csv"));
-            CSVReader csvreader = new CSVReader(reader);
-
-            List<String[]> etiquetas = csvreader.readAll(); // Leo archivo
-            etiquetas.remove(0); // Elimino la primera fila que informa del nombre de las columnas
-
-            csvreader.close(); // Cerramos csv
-
-            // Índice
-            IndexBuilder i = new IndexBuilder("./rquestions/Questions.csv", "./rquestions/Answers.csv", etiquetas);
-        }
+        
         // Búsqueda
         while(true){
             System.out.println("Consulta?: ");
@@ -47,8 +28,9 @@ public class StackOverflowSearcher {
                 break;
             }
             
-            iS.getResultSearch();
-            iS.getResultFacet();
+            System.out.println(iS.getResultSearch().toString());
+            System.out.println("-------------------------------------------------------------------------");
+            System.out.println(iS.getResultFacet());
             
             if(line.equals("")){
                 break;
