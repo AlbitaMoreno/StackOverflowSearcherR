ef95ef0a37b0437eae7208297552a413a740fe40
albertosml <albertosml@correo.ugr.es>
2018-12-05 15:37:49 +0100

    cambios

--- StackOverflowSearch/src/stackoverflowsearcher/ProcessQuery.java
+++ StackOverflowSearch/src/stackoverflowsearcher/ProcessQuery.java
@@ -1,10 +1,7 @@
 package stackoverflowsearcher;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
